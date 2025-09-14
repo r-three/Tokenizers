a1c98d5443da0fb09d68d945c9ea3c9855c0132c
Gül Sena Altıntaş <30217912+gsaltintas@users.noreply.github.com>
2025-09-13 23:43:26 -0400

    Updated model name and tokenizer mismatch on eval script

--- slurm_scripts/eval_supertoken_on_completion.sh
+++ slurm_scripts/eval_supertoken_on_completion.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=eval_supertoken_models
 #SBATCH --output=/project/aip-craffel/gsa/.slurm/%j.out
-#SBATCH --gres=gpu:l40s:3
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=3
+#SBATCH --gres=gpu:l40s:4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=52G
+#SBATCH --mem=58G
 #SBATCH --time=06:00:00
 #SBATCH --account=aip-craffel
 
 # sbatch /home/gsa/tokenizers/slurm_scripts/eval_supertoken_models.sh
-models=( "common-pile-comma-v0.1" "meta-llama-Llama-3.2-1B" "microsoft-Phi-3-mini-4k-instruct" "gpt2" "bigscience-bloom" "facebook-xglm-564M"  "mistralai-tekken" "google-byt5-small" "google-bert-bert-base-multilingual-cased" "Qwen-Qwen3-8B"  "tokenmonster-englishcode-32000-consistent-v1"  "tiktoken-gpt-4o"  )
-tokenizers=("google/gemma-2-2b" "common-pile/comma-v0.1-1t" "meta-llama/Llama-3.2-1B" "microsoft/Phi-3-mini-4k-instruct" "gpt2" "bigscience/bloom" "facebook/xglm-564M"  "mistralai/tekken" "google/byt5-small" "google-bert/bert-base-multilingual-cased" "Qwen/Qwen3-8B"  "tokenmonster/englishcode-32000-consistent-v1"  "tiktoken/gpt-4o"  )    
+models=("google-gemma-2-2b" "common-pile-comma-v0.1" "meta-llama-Llama-3.2-1B" "microsoft-Phi-3-mini-4k-instruct" "gpt2" "bigscience-bloom" "facebook-xglm-564M" "mistralai-tekken" "google-byt5-small" "google-bert-bert-base-multilingual-cased" "Qwen-Qwen3-8B" "tokenmonster-englishcode-32000-consistent-v1" "tiktoken-gpt-4o")
+tokenizers=("google/gemma-2-2b" "common-pile/comma-v0.1-1t" "meta-llama/Llama-3.2-1B" "microsoft/Phi-3-mini-4k-instruct" "gpt2" "bigscience/bloom" "facebook/xglm-564M" "mistralai/tekken" "google/byt5-small" "google-bert/bert-base-multilingual-cased" "Qwen/Qwen3-8B" "tokenmonster/englishcode-32000-consistent-v1" "tiktoken/gpt-4o")
 
-
-# models=( "tokenmonster-englishcode-32000-consistent-v1" )
-# tokenizers=(  "tokenmonster/englishcode-32000-consistent-v1" )
-
+models=("tokenmonster-englishcode-32000-consistent-v1")
+tokenizers=("tokenmonster/englishcode-32000-consistent-v1")
 
 MEM="14G"
 # Load necessary modules
@@ -34,49 +32,40 @@ TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_math,tokenizer_ro
 TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_math,tokenizer_robustness_completion_italian,tokenizer_robustness_completion_chinese"
 # TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_english"
 # TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_stem"
-# TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_italian"
 # TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_chinese"
+# TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_italian"
+TOKENIZATION_ROBUSTNESS_TASKS="tokenizer_robustness_completion_math"
 
 EXTRA_BENCHMARKS=""
 
-
-cnt=$((${#models[@]}-1))
+cnt=$((${#models[@]} - 1))
 echo Will evaluate $cnt models
-for i in $(seq 0 $cnt); do 
-echo $i
-model="${models[i]}"
-tokenizer="${tokenizers[i]}"
-model_name="r-three/toksuite"
-model_path="$model_name/$model/"
-hf_model_path="$PROJECT/models/$model_name"
-tokenizer_path="$PROJECT/tokenizers/$tokenizer"
-model_name="supertoken_models-llama_$model"
-hf_out_path="r-three/supertoken_models-llama_$model"
-tokenizer_name="blester125/supervocab-$model"
-    echo "Model: $model"
-    echo "Tokenizer: $tokenizer"
+for i in $(seq 0 $cnt); do
+	echo $i
+	model="${models[i]}"
+	tokenizer="${tokenizers[i]}"
+	model_name="r-three/toksuite"
+	model_name="supertoken_models-llama_$model"
+	hf_out_path="r-three/supertoken_models-llama_$model"
+	echo "Model: $model"
+	echo "Tokenizer: $tokenizer"
 	args=" "
-    run_name="$(basename $model)-$(date +%s)"
+	run_name="$(basename $model)-$(date +%s)"
 
-    wandb_args=""
-    # wandb_args=" --wandb_args project=supertoken-evaluation-paper,name=$run_name "
-    wandb_args=" --wandb_args project=toksuite-evals,entity=raffel-reports,group=$model_name,name=$run_name "
-    common_args=" --model hf --model_args pretrained=${hf_out_path},tokenizer=${tokenizer},trust_remote_code=true --device cuda --verbosity DEBUG ${wandb_args} ${args} "
+	wandb_args=""
+	# wandb_args=" --wandb_args project=supertoken-evaluation-paper,name=$run_name "
+	# wandb_args=" --wandb_args project=toksuite-evals,entity=raffel-reports,group=$model_name,name=$run_name "
+	common_args=" --model hf --model_args pretrained=${hf_out_path},tokenizer=${tokenizer},trust_remote_code=true --device cuda --verbosity DEBUG ${wandb_args} ${args} "
 
-# Run lm_eval with converted model
-echo "Running lm_eval..."
+	# Run lm_eval with converted model
+	echo "Running lm_eval..."
 
-
-srun --exclusive --gres=gpu:1 --mem $MEM -o $OUT_DIR/logs/$run_name.log -e $OUT_DIR/logs/$run_name.err --nodes=1 --ntasks=1  bash -c \
-' lm_eval $common_args --log_samples  \
---tasks $TOKENIZATION_ROBUSTNESS_TASKS \
---output_path $OUT_DIR ' 
-
-# srun --exclusive --gres=gpu:1 --nodes=1 --ntasks=1 --mem $MEM 
-# lm_eval \
-# --tasks $EXTRA_BENCHMARKS \
-# --output_path "${OUT_DIR}" --confirm_run_unsafe_code $common_args $wandb_args  &
-
+	# srun --ntasks=1 --nodes=1 --gres=gpu:1 \
+	# 	--exclusive --mem=$MEM \
+	# 	-o $OUT_DIR/logs/$run_name.log -e $OUT_DIR/logs/$run_name.err \
+	lm_eval $common_args --log_samples \
+		--tasks $TOKENIZATION_ROBUSTNESS_TASKS \
+		--output_path $OUT_DIR &
 done
 wait
 
